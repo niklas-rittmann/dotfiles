62567e1ff320baacb440181f710183eef7b81ce2
Niklas Rittmann <niklas.rittmann@capgemini.com>
2022-03-24 17:33:42 +0100

    chore: Add keybinds for trivy

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -16,6 +16,11 @@ alias gp "git push"
 # alias for nvim
 alias v "nvim"
 
+# trivy 
+# e. g. trivy image > output.txt
+alias ty20 "docker run --rm -v /var/run/docker.sock:/var/run/docker.sock aquasec/trivy:0.20.1"
+alias ty16 "docker run --rm -v /var/run/docker.sock:/var/run/docker.sock aquasec/trivy:0.16.0"
+
 # Source direnv on loading
 direnv hook fish | source
 
